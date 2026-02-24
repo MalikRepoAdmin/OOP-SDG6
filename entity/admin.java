e9f29b010664293b9e421f4dbdad99122971c986
zahrah888 <zzzzzhrrr@gmail.com>
2026-02-24 14:53:34 +0700

    Update admin.java

--- entity/admin.java
+++ entity/admin.java
@@ -1,32 +1,14 @@
 public class Admin {
-    String id_admin, email;
-    String password;
+    private String idAdmin;
+    private String email = "zzzzzhrrr@gmail.com";
+    private String password = "admin123";
 
-    public void set_email(String email){
-        this.email = email;
+    public Admin(String idAdmin) {
+        this.idAdmin = idAdmin;
     }
 
-    public void set_id(String id){
-        this.id_admin = id;
-    }
-
-    public void set_pass(String password){
-        this.password = password;
-    }
-
-    public void showdata(){
-        System.out.println("ID admin : " +id_admin);
-        System.out.println("Email : " +email);
-        System.out.println("Password : " +password);
-    }
-
-    public static void main (String[] args){
-        Admin adminobj = new Admin();
-        
-        adminobj.set_id("A001");
-        adminobj.set_email("admin123@gmail.com");
-        adminobj.set_pass("admin123");
-
-        adminobj.showdata();
+    public void showData() {
+        System.out.println("ID Admin : " + idAdmin);
+        System.out.println("Email    : " + email);
     }
 }
