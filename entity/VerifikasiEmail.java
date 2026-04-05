c1b224de7b2f027f456628251c35b58a8848e38c
Angelis Nasta Nadia <nadiaangelis25@gmail.com>
2026-04-05 18:17:30 +0700

    Refactor VerifikasiEmail class with additional user fields

--- entity/VerifikasiEmail.java
+++ entity/VerifikasiEmail.java
@@ -1,36 +1,67 @@
 package entity;
 
 public class VerifikasiEmail extends User {
-    public int idVerifikasi;
-    public String tglKirim;
-    public String tglVerifikasi;
-    public String statusEmail;
 
-    // Constructor
-    public VerifikasiEmail(
-                           int idVerifikasi, String tglKirim, String tglVerifikasi, String statusEmail) {
+    private int idVerifikasi;
+    private String tglKirim;
+    private String tglVerifikasi;
+    private String statusEmail;
+
+    public VerifikasiEmail(int idUser, String nama, String email, String password,
+                           String tanggalLahir, String pekerjaan, String bio,
+                           String domisili, String fotoProfil,
+                           int idVerifikasi, String tglKirim,
+                           String tglVerifikasi, String statusEmail) {
+
+        super(idUser, nama, email, password, tanggalLahir,
+              pekerjaan, bio, domisili, fotoProfil);
+
         this.idVerifikasi = idVerifikasi;
         this.tglKirim = tglKirim;
         this.tglVerifikasi = tglVerifikasi;
         this.statusEmail = statusEmail;
     }
 
-    // Getter & Setter (opsional)
-    public int getIdVerifikasi() { return idVerifikasi; }
-    public void setIdVerifikasi(int idVerifikasi) { this.idVerifikasi = idVerifikasi; }
+    public int getIdVerifikasi() {
+        return idVerifikasi;
+    }
 
-    public String getTglKirim() { return tglKirim; }
-    public void setTglKirim(String tglKirim) { this.tglKirim = tglKirim; }
+    public void setIdVerifikasi(int idVerifikasi) {
+        this.idVerifikasi = idVerifikasi;
+    }
 
-    public String getTglVerifikasi() { return tglVerifikasi; }
-    public void setTglVerifikasi(String tglVerifikasi) { this.tglVerifikasi = tglVerifikasi; }
+    public String getTglKirim() {
+        return tglKirim;
+    }
 
-    public String getStatusEmail() { return statusEmail; }
-    public void setStatusEmail(String statusEmail) { this.statusEmail = statusEmail; }
+    public void setTglKirim(String tglKirim) {
+        this.tglKirim = tglKirim;
+    }
 
-    // Method tampilkan data
+    public String getTglVerifikasi() {
+        return tglVerifikasi;
+    }
+
+    public void setTglVerifikasi(String tglVerifikasi) {
+        this.tglVerifikasi = tglVerifikasi;
+    }
+
+    public String getStatusEmail() {
+        return statusEmail;
+    }
+
+    public void setStatusEmail(String statusEmail) {
+        this.statusEmail = statusEmail;
+    }
+
     public void tampilkanData() {
-        System.out.println("\n\nID Verifikasi  : " + idVerifikasi);
+        System.out.println("\n=== DATA USER ===");
+        System.out.println("ID User        : " + getIdUser());
+        System.out.println("Nama           : " + getNama());
+        System.out.println("Email          : " + getEmail());
+
+        System.out.println("\n=== VERIFIKASI EMAIL ===");
+        System.out.println("ID Verifikasi  : " + idVerifikasi);
         System.out.println("Tanggal Kirim  : " + tglKirim);
         System.out.println("Tanggal Verif  : " + tglVerifikasi);
         System.out.println("Status Email   : " + statusEmail);
