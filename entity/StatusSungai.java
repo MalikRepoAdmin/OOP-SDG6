d32813eca123d84f39db6fb0f2693b618cd453b0
RaditDP <radityadp7224@gmail.com>
2026-04-07 14:02:26 +0700

    Fix the setter method and update the method name

--- entity/StatusSungai.java
+++ entity/StatusSungai.java
@@ -2,7 +2,7 @@ package entity;
 
 public class StatusSungai extends Sungai { // child class 2
 
-    public void Status(String status) {
+    public void tampilStatus() {
         System.out.println("Status Sungai : " + getStatus());
     }
 
