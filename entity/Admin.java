d34b23e40b4c0fa22bef5d3bd48dc121cb376e70
zahrah888 <zzzzzhrrr@gmail.com>
2026-04-21 21:08:47 +0700

    Update Admin.java
    mengimplementasikan parameter konstruktor dan interface actionable untuk menginput data secara dinamis dlm konstruktornya

--- entity/Admin.java
+++ entity/Admin.java
@@ -1,25 +1,22 @@
 package entity;
 
-public class Admin extends User {
+public class Admin extends User implements Actionable {
     private String idAdmin;
 
-    public Admin(String idAdmin) {
-                            
-        super(2, "", "", "", "",
-              "", "", "", "");
-        this.idAdmin = "ADM001";
-        this.setEmail("zzzzzhrrr@gmail.com");
-        this.setPassword("admriverX12");
-        this.setNama("Zahrah");
+    public Admin(String idAdmin, String nama, String email, String password) {
+        super(2, nama, email, password, "", "", "", "", ""); 
+        this.idAdmin = idAdmin;
+        
+        this.setNama(nama);
+        this.setEmail(email);
+        this.setPassword(password);
     }
 
-    public String getIdAdmin() {
-        return idAdmin;
-    }
-
-    public void showDataAdmin() {
+    @Override
+    public void displayInfo() {
+        System.out.println("=== DATA ADMIN (LOGGED IN) ===");
         System.out.println("ID Admin : " + idAdmin);
-        System.out.println("Email    : " + getEmail());
         System.out.println("Nama     : " + getNama());
+        System.out.println("Email    : " + getEmail());
     }
 }
