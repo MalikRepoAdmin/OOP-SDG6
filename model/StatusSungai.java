b2164d7ca512fb0a0a3633f9a35c5ad4d0002586
zahrah888 <zzzzzhrrr@gmail.com>
2026-05-26 11:54:37 +0700

    refactor(model): memisahkan komponen pada entity StatusSungai menjadi kelas data mandiri demi memenuhi prinsip arsitektur SRP

--- model/StatusSungai.java
+++ model/StatusSungai.java
@@ -1,9 +1,21 @@
 package model;
 
-public class StatusSungai extends Sungai {
+// Memenuhi SRP: Kelas berdiri mandiri khusus mengelola parameter kualitas fisik air
+public class StatusSungai {
+    // Enkapsulasi data dengan modifier private
+    private double indeksPencemaran; 
+    private String kondisiFisikAir; 
 
-    public StatusSungai(int id, String namaSungai, String lokasi, String kategori, String status) {
-        super(id, namaSungai, lokasi, kategori, status);
+    // Constructor murni pengisian parameter data status kualitas
+    public StatusSungai(double indeksPencemaran, String kondisiFisikAir) {
+        this.indeksPencemaran = indeksPencemaran;
+        this.kondisiFisikAir = kondisiFisikAir;
     }
-}
 
+    // Metode Getter dan Setter untuk pembatasan hak akses variabel
+    public double getIndeksPencemaran() { return indeksPencemaran; }
+    public void setIndeksPencemaran(double indeksPencemaran) { this.indeksPencemaran = indeksPencemaran; }
+
+    public String getKondisiFisikAir() { return kondisiFisikAir; }
+    public void setKondisiFisikAir(String kondisiFisikAir) { this.kondisiFisikAir = kondisiFisikAir; }
+}
