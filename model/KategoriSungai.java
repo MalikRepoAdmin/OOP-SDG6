20d3ed30758db3473f11cebc76521d29bbe0fc6c
zahrah888 <zzzzzhrrr@gmail.com>
2026-05-26 10:05:49 +0700

    feat(model): menambahkan model kategori sungai sebagai class child dari sungai + getter setter untuk mengelola tipe kelayakan air

---
+++ model/KategoriSungai.java
@@ -0,0 +1,8 @@
+package model;
+
+public class KategoriSungai extends Sungai {
+
+    public KategoriSungai(int id, String namaSungai, String lokasi, String kategori, String status) {
+        super(id, namaSungai, lokasi, kategori, status);
+    }
+}
