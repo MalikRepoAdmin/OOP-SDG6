4e7c47460effa3afff68d00f80997a9e2955e71f
zahrah888 <zzzzzhrrr@gmail.com>
2026-05-26 11:22:23 +0700

    feat: mendesain kontrak pada ICommentCreator untuk mengisolasi perilaku pembuatan instansi komentar baru berdasarkan identitas pengguna

---
+++ model/ICommentCreator.java
@@ -0,0 +1,6 @@
+package model;
+
+//Konsep Interface Segregation Principle (kontrak aksi yg terisolasi khusus pembuatan data
+public interface ICommentCreator {
+    void addComment(String comment,String userId);
+}
