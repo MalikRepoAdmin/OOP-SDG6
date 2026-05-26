c77a897fb23c14fcbe9e040a56783fbcd39b5580
zahrah888 <zzzzzhrrr@gmail.com>
2026-05-26 11:47:08 +0700

    refactor(model): memisahkan komponen KategoriSungai menjadi kelas data mandiri demi mematuhi prinsip arsitektur SRP

--- model/KategoriSungai.java
+++ model/KategoriSungai.java
@@ -1,8 +1,21 @@
 package model;
 
-public class KategoriSungai extends Sungai {
+// Memenuhi SRP: Kelas berdiri mandiri khusus mengelola standarisasi tingkatan keamanan air
+public class KategoriSungai {
+    // Enkapsulasi data dengan modifier private
+    private String tingkatanKeamanan; 
+    private String deskripsiKelayakan;
 
-    public KategoriSungai(int id, String namaSungai, String lokasi, String kategori, String status) {
-        super(id, namaSungai, lokasi, kategori, status);
+    // Constructor murni pengisian parameter data kategori
+    public KategoriSungai(String tingkatanKeamanan, String deskripsiKelayakan) {
+        this.tingkatanKeamanan = tingkatanKeamanan;
+        this.deskripsiKelayakan = deskripsiKelayakan;
     }
+
+    // Metode Getter dan Setter untuk pembatasan hak akses variabel
+    public String getTingkatanKeamanan() { return tingkatanKeamanan; }
+    public void setTingkatanKeamanan(String tingkatanKeamanan) { this.tingkatanKeamanan = tingkatanKeamanan; }
+
+    public String getDeskripsiKelayakan() { return deskripsiKelayakan; }
+    public void setDeskripsiKelayakan(String deskripsiKelayakan) { this.deskripsiKelayakan = deskripsiKelayakan; }
 }
