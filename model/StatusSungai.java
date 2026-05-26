8a3cb9732e9897bd231ef41b5f2e35505116b643
zahrah888 <zzzzzhrrr@gmail.com>
2026-05-26 10:08:28 +0700

    feat(model): menambahkan model StatusSungai sebagai class child dari ungai + getter setter, beraksi mengelola parameter kualitas air sungai

---
+++ model/StatusSungai.java
@@ -0,0 +1,9 @@
+package model;
+
+public class StatusSungai extends Sungai {
+
+    public StatusSungai(int id, String namaSungai, String lokasi, String kategori, String status) {
+        super(id, namaSungai, lokasi, kategori, status);
+    }
+}
+
