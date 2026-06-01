e96c68d41a2fb01ffd97469534aaacd902fcb774
Angelis Nasta Nadia <nadiaangelis25@gmail.com>
2026-06-01 10:35:04 +0700

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,11 @@
+public import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public static List<User> users = new ArrayList<>();
+    public static List<Sungai> sungaiList = new ArrayList<>();
+    public static List<Feed> feeds = new ArrayList<>();
+    public static List<Komentar> komentarList = new ArrayList<>();
+} {
+    
+}
