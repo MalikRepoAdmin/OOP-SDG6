32db421d7907471669275bc36608ffb8c9e8c5b7
MalikRepoAdmin <mohamadmalikfajarbaihaqi@gmail.com>
2026-06-09 14:59:29 +0700

    ADD: Validation for new field "Alasan Prioritas"

--- src/main/java/views/sungai/SungaiAddRequest.java
+++ src/main/java/views/sungai/SungaiAddRequest.java
@@ -12,12 +12,17 @@ public class SungaiAddRequest {
 	@NotBlank(message = "Status Sungai Harus Diisi")
 	private String status;
 
+	@NotBlank(message = "Alasan Prioritas harus diisi")
+	private String alasanPrioritas;
+
 
 	public String getNamaSungai() { return this.namaSungai; }
 	public String getKategori() { return this.kategori; }
 	public String getStatus() { return this.status; }
+	public String getAlasanPrioritas() { return this.alasanPrioritas; }
 
 	public void setNamaSungai(String namaSungai) { this.namaSungai = namaSungai; }
 	public void setKategori(String kategori) { this.kategori = kategori; }
 	public void setStatus(String status) { this.status = status; }
+	public void setAlasanPrioritas(String alasanPrioritas) { this.alasanPrioritas = alasanPrioritas; }
 }
